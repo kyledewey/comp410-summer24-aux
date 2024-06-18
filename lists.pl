da62153fe08269811bf569f16f0c585914099ea4
Kyle Dewey <kyle.dewey@csun.edu>
2024-06-18 11:37:12 -0700

    code from class

--- lists.pl
+++ lists.pl
@@ -11,8 +11,39 @@
 %     rest = append(list1.tail, list2) # append([2, 3], [4, 5, 6])  ==> [2, 3, 4, 5, 6]
 %     return Cons(list1.head, rest) # [1, 2, 3, 4, 5, 6]
 myAppend([], List2, List2).
-myAppend([Head|Tail], List2, Output) :-
-    myAppend(Tail, List2, Rest),
-    Output = [Head|Rest].
+myAppend([Head|Tail], List2, [Head|Rest]) :-
+    myAppend(Tail, List2, Rest).
 
-% TODO: cleanup, run, trace
+% contains: List, Element
+% contains([], Element) :- fail.
+contains([Head|_], Head).
+contains([_|Tail], Element) :-
+    % Head \= Element,
+    contains(Tail, Element).
+    
+    
+sumAll([], 0).
+sumAll([H|Tail], Sum) :-
+    sumAll(Tail, NewSum),
+    Sum is NewSum + H.
+
+sumAllAccum(List, Result) :-
+    sumAllAccum(List, 0, Result).
+
+sumAllAccum([], Accum, Accum).
+sumAllAccum([Head|Tail], Accum, Result) :-
+    NewAccum is Accum + Head,
+    sumAllAccum(Tail, NewAccum, Result).
+
+myReverse(List, Result) :-
+    myReverse(List, [], Result).
+
+myReverse([], Accum, Accum).
+myReverse([Head|Tail], Accum, Result) :-
+    myReverse(Tail, [Head|Accum], Result).
+
+sublist([], []).
+sublist([Head|Tail], [Head|Sublist]) :- % keep Head
+    sublist(Tail, Sublist).
+sublist([_|Tail], Sublist) :- % discard Head
+    sublist(Tail, Sublist).
--- recursion.pl
+++ recursion.pl
@@ -42,3 +42,85 @@ myBetween2(Low, High, X) :-
     (Low < High,
      Next is Low + 1,
      myBetween2(Next, High, X)).
+
+% plus(integer(5), integer(7))
+% minus(integer(4), integer(2))
+%
+% arithEval: AST, Integer
+arithEval(integer(X), X).
+arithEval(plus(E1, E2), Result) :-
+    % arithEval(E1) + arithEval(E2)
+    arithEval(E1, E1Result),
+    arithEval(E2, E2Result),
+    Result is E1Result + E2Result.
+arithEval(minus(E1, E2), Result) :-
+    % arithEval(E1) + arithEval(E2)
+    arithEval(E1, E1Result),
+    arithEval(E2, E2Result),
+    Result is E1Result - E2Result.
+
+% writeAllInRange(Min, Max)
+% ?- writeAllInRange(3, 3).
+% 3
+% true.
+% ?- writeAllInRange(4, 8).
+% 4
+% 5
+% 6
+% 7
+% 8
+% true.
+% ?- writeAllInRange(3, 2).
+% fail.
+
+writeAllInRange(Min, Max) :-
+    Min < Max,
+    writeln(Min),
+    Next is Min + 1,
+    writeAllInRange(Next, Max).
+writeAllInRange(Max, Max) :-
+    writeln(Max).
+
+
+evensBetween(Min, Max, Min) :-
+    Min =< Max,
+    0 is mod(Min, 2).
+evensBetween(Min, Max, Num) :-
+    Min < Max,
+    NewMin is Min + 1,
+    evensBetween(NewMin, Max, Num).
+
+productAll(X, X, X).
+productAll(Min, Max, Result) :-
+    Min < Max,
+    NewMin is Min + 1,
+    productAll(NewMin, Max, Rest),
+    Result is Min * Rest.
+
+% tailrecProductAll(Min, Max, Result)
+tailrecProductAll(Min, Max, Result) :-
+    tailrecProductAll(Min, Max, 1, Result).
+
+% tailrecProductAll(Min, Max, Accum, Result)
+tailrecProductAll(Min, Min, Accum, NewAccum) :-
+    NewAccum is Min * Accum.
+tailrecProductAll(Min, Max, Accum, Result) :-
+    Min < Max,
+    NewMin is Min + 1,
+    NewAccum is Min * Accum,
+    tailrecProductAll(NewMin, Max, NewAccum, Result).
+
+element(gallium).
+element(chlorine).
+
+mp(water, 0).
+mp(gallium, 30).
+mp(chlorine, -102).
+
+bp(water, BP) :-
+    mp(water, MP),
+    BP is MP + 100.
+bp(gallium, 2400).
+bp(chlorine, -34).
+
+% ?- mp(E, MP), element(E), MP =< 0.
